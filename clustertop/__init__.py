d942a4926b1eea44cf2aaeb2592d7e805dc149dd
Ross <rossdylan@csh.rit.edu>
2014-08-05 19:07:12 -0400

    You can now select the poller you want to use via the config file

--- clustertop/__init__.py
+++ clustertop/__init__.py
@@ -2,6 +2,7 @@ from clustertop.poller import Poller
 from clustertop.bootstrap import bootstrapper
 from ConfigParser import ConfigParser
 import argparse
+import importlib
 
 
 def main():
@@ -16,12 +17,17 @@ def main():
     user = cf.get('main', 'zabbix_user')
     passwd = cf.get('main', 'zabbix_pass')
     hosts = cf.get('main', 'hosts').split(',')
+    the_poller = Poller
+    if cf.has_option('main', 'poller'):
+        mod_path, cls = cf.get('main', 'poller').split(':')
+        module = importlib.import_module(mod_path)
+        the_poller = getattr(module, cls)
 
     if args.command == 'bootstrap':
         bootstrapper(zhost, user, passwd, hosts)
     elif args.command == 'run':
-        poller = Poller(cf)
+        poller = the_poller(cf)
         poller.poll_loop()
     elif args.command == 'check':
-        poller = Poller(cf)
+        poller = the_poller(cf)
         poller.poll()
